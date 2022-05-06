0014d30f38918b0265519a8e195b3ad988418047
Florence.Mary <Florence.Mary@mirrhia.com>
2022-05-06 14:18:08 +0200

    Coffee machine using functions

--- src/main/kotlin/com/play/with/kotlin/CoffeeMachine.kt
+++ src/main/kotlin/com/play/with/kotlin/CoffeeMachine.kt
@@ -1,33 +1,87 @@
 package com.play.with.kotlin
 
+var currentWater = 400
+var currentMilk = 540
+var currentBeans = 120
+var currentCups = 9
+var currentMoney = 550
+
 fun main() {
-    val neededWater = 200
-    val neededMilk = 50
-    val neededBeans = 15
+    printMachineState()
+    askAction()
+    printMachineState()
+}
 
-    print("Write how many ml of water the coffee machine has:")
-    val actualWater = readLine()!!.toInt()
-    print("Write how many ml of milk the coffee machine has:")
-    val actualMilk = readLine()!!.toInt()
-    print("Write how many grams of coffee beans the coffee machine has:")
-    val actualBeans = readLine()!!.toInt()
-    print("Write how many cups of coffee you will need:")
-    val coffeesAsked = readLine()!!.toInt()
+fun printMachineState() {
+    print(
+        """
+            The coffee machine has:
+            $currentWater ml of water
+            $currentMilk ml of milk
+            $currentBeans g of coffee beans
+            $currentCups disposable cups
+            $$currentMoney of money
+        """.trimIndent()
+    )
+}
 
-   if (coffeesAsked * neededWater <= actualWater && coffeesAsked * neededBeans <= actualBeans && coffeesAsked * neededMilk <= actualMilk) {
-        val waterOverflow = (actualWater - (coffeesAsked * neededWater)) / neededWater
-        val beansOverflow = (actualBeans - (coffeesAsked * neededBeans)) / neededBeans
-        val milkOverflow = (actualMilk - (coffeesAsked * neededMilk)) / neededMilk
-        val overflow = minOf(waterOverflow, milkOverflow, beansOverflow)
-        print("Yes, I can make that amount of coffee")
-        if (overflow >= 1) print(" (and even $overflow more than that)")
-    } else {
-        val waterCapacity = actualWater / neededWater
-        val beansCapacity = actualBeans / neededBeans
-        val milkCapacity = actualMilk / neededMilk
-        val capacity = minOf(waterCapacity, beansCapacity, milkCapacity)
-        print("No, I can make only $capacity cups of coffee")
+fun makeEspresso() {
+    currentMoney += 4
+    currentWater -= 250
+    currentBeans -=16
+}
+
+fun makeLatte() {
+    currentMoney += 7
+    currentWater -= 350
+    currentMilk -= 75
+    currentBeans -=20
+}
+
+fun makeCappuccino() {
+    currentMoney += 6
+    currentWater -= 200
+    currentMilk -= 100
+    currentBeans -=12
+}
+
+fun askAction() {
+    println("Write action (buy, fill, take):")
+    val userInput = readLine()!!
+    when (userInput) {
+        "buy" -> askChoice()
+        "fill" -> fillMachine()
+        "take" -> takeMoney()
     }
+}
 
+fun fillMachine() {
+    print("Write how many ml of water do you want to add:")
+    val addWater = readLine()!!.toInt()
+    currentWater += addWater
+    print("Write how many ml of milk do you want to add:")
+    val addMilk = readLine()!!.toInt()
+    currentMilk += addMilk
+    print("Write how many grams of coffee beans do you want to add:")
+    val addBeans = readLine()!!.toInt()
+    currentBeans += addBeans
+    print("Write how many disposable cups of coffee do you want to add:")
+    val addCups = readLine()!!.toInt()
+    currentCups += addCups
+}
 
+fun takeMoney() {
+    print("I gave you $${currentMoney}")
+    currentMoney = 0
+}
+
+fun askChoice() {
+    println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+    val userChoice = readLine()!!.toInt()
+    when (userChoice) {
+        1 -> makeEspresso()
+        2 -> makeLatte()
+        3 -> makeCappuccino()
+    }
+    --currentCups
 }
